01d3b96b2f9def993764072b3b5ca93463416d5c
Joseph Adams <joseph@josephadams.net>
2019-04-16 13:29:51 +0100

    Remove unnecessary body tag from ui.R

--- shiny_app_with_cookie_control/ui.R
+++ shiny_app_with_cookie_control/ui.R
@@ -12,10 +12,8 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  tags$body(
-    HTML("<script src='https://cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js'></script>"),
-    tags$script(src = "cookie_control_config.js")
-  ),
+  HTML("<script src='https://cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js'></script>"),
+  tags$script(src = "cookie_control_config.js"),
   
   # Application title
   titlePanel("Old Faithful Geyser Data"),
